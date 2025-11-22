41f3c7664a24a16e6244cc34c186f2887cf3d5ca
Hiroten31 <hiroten31@gmail.com>
2025-11-22 23:37:07 +0100

    Clean up before changing to Spring Boot model

--- public/js/nav-panel.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const arrow = document.getElementById("arrow");
-
-    arrow.addEventListener("click", () => {
-        const html = document.documentElement;
-        html.classList.toggle("nav-collapsed");
-        localStorage.setItem("navPanelCollapsed", html.classList.contains("nav-collapsed"));
-    });
-});
